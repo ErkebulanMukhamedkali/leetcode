db51dc1aab863d2ff53b270b0f3a6f627ef21c92
Yerkebulan Mukhamedkali <yerkebulan.mukhamedkali@MacBook-Air-Yerkebulan.local>
2022-06-05 16:58:47 +0600

    Got rid of n^2

--- twoSum/main.go
+++ twoSum/main.go
@@ -3,13 +3,8 @@ package main
 import "log"
 
 func main() {
-	var nums = [][]int{
-		{2, 7, 11, 15}, {3, 2, 4}, {3, 3},
-		{3, 2, 3},
-	}
-	var targets = []int{
-		9, 6, 6,
-		6}
+	var nums = [][]int{{2, 7, 11, 15}, {3, 2, 4}, {3, 3}, {3, 2, 3}}
+	var targets = []int{9, 6, 6, 6}
 
 	for i := range targets {
 		log.Println(twoSum(nums[i], targets[i]))
@@ -21,14 +16,14 @@ func twoSum(nums []int, target int) []int {
 		return []int{0, 1}
 	}
 
-	for i, num1 := range nums {
-		j := i + 1
-		for _, num2 := range nums[j:] {
-			if num1+num2 == target {
-				return []int{i, j}
-			}
-			j++
+	buffer := make(map[int]int)
+
+	for i, num := range nums {
+		if index, ok := buffer[target-num]; ok {
+			return []int{index, i}
 		}
+		buffer[num] = i
+
 	}
 	return nil
 }
