9d2cd9722c0d8aa69ddd2e8d31156849b7b81ce0
YerkebulanMukhamedkali <erkebulan.mukhamedkali@gmail.com>
2023-08-27 18:15:17 +0600

    twoSum 2

--- twoSum/main.go
+++ twoSum/main.go
@@ -1,5 +1,11 @@
 package main
 
+import "log"
+
+func main() {
+	log.Println(twoSum2([]int{2, 3, 4}, 6))
+}
+
 func twoSum(nums []int, target int) []int {
 	if len(nums) == 2 {
 		return []int{0, 1}
@@ -15,4 +21,16 @@ func twoSum(nums []int, target int) []int {
 
 	}
 	return nil
+}
+
+func twoSum2(numbers []int, target int) []int {
+	i, j := 0, len(numbers)-1
+	for numbers[i]+numbers[j] != target {
+		if numbers[j]+numbers[i] > target {
+			j -= 1
+			continue
+		}
+		i -= -1
+	}
+	return []int{i + 1, j + 1}
 }
